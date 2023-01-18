aaf3cb6362f43afbf4d1a387d6ee3faf6812990d
admond <admondtamang@gmail.com>
2023-01-18 11:45:53 +0545

    feat: clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,16 @@ export default function Document() {
         <script
           src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GTM}`}
           type="text/partytown"
-          defer
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "fg2zxz06pg");`,
+          }}
         />
       </Head>
       <body>
