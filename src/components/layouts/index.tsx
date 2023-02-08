57de08fa6596c840f1d6f0bfaa5d780a02aff522
admond <admondtamang@gmail.com>
2023-02-08 23:12:11 +0545

    add: fade effect for night mode

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./header";
 export default function Layout({ children }: any) {
   return (
-    <main className="bg-white font-mono dark:bg-slate-800 ">
+    <main className="bg-white font-mono transition-all duration-700 dark:bg-slate-800">
       <Header />
       {children}
     </main>
